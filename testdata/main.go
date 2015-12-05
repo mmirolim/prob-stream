27d7b44308a6a3b1f0f9266b9af7d97ff2561ccf
mmirolim <mirolim.mirzakhmedov@regium.com>
2015-12-05 15:31:13 +0300

    genEvent func

--- testdata/main.go
+++ testdata/main.go
@@ -7,7 +7,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"time"
+
+	"github.com/leesper/go_rng"
 )
 
 var (
@@ -63,17 +66,36 @@ func init() {
 }
 
 func main() {
+	// process weights
+	pids = enableWeights(pids)
+	utmSDict = enableWeights(utmSDict)
+	utmMDict = enableWeights(utmMDict)
 
+	fmt.Println("pids", pids)
+}
+
+// TODO invert index? or use probability for each item
+// to make list weighted copy number of items in list according to weight
+func enableWeights(tags []Tag) []Tag {
+	var dst []Tag
+	for _, v := range tags {
+		for i := 0; i < v.Weight-1; i++ {
+			dst = append(dst, Tag{v.Word, 1})
+		}
+	}
+
+	return dst
 }
 
 // generates events with normal distribution with weights
 func genNormalDistEvent(pids []Tag, utmms []Tag, utmss []Tag) Event {
-	var pid, utmm, utms string
+	// tags will have uniform distribution
+	uniProb := rng.NewUniformGenerator(time.Now().UnixNano())
 	ts := time.Now().UTC().Unix()
 	return Event{
-		Pid:       pid,
-		UtmM:      utmm,
-		UtmS:      utms,
+		Pid:       pids[uniProb.Int64n(int64(len(pids)))].Word,
+		UtmM:      utmms[uniProb.Int64n(int64(len(utmms)))].Word,
+		UtmS:      utmss[uniProb.Int64n(int64(len(utmss)))].Word,
 		TimeStamp: ts,
 	}
 }
